998ec41745ecb0020dc4392c583fb55fd53b11f4
Manzar-Hassan <manzarhassan05@gmail.com>
2024-03-12 22:51:24 +0530

    adds sort colors

---
+++ TWO_POINTERS/sortColors.js
@@ -0,0 +1,30 @@
+function swapColors(colors, firstPointer, secondPointer) {
+    let tempColor = colors[firstPointer];
+    colors[firstPointer] = colors[secondPointer];
+    colors[secondPointer] = tempColor;
+}
+
+function sortColors(colors) {
+    let redPointer = 0;
+    let whitePointer = 0;
+    let bluePointer = colors.length - 1;
+
+    while (whitePointer <= bluePointer) {
+        if (colors[whitePointer] === 0) {
+            swapColors(colors, whitePointer, redPointer);
+            redPointer++;
+            whitePointer++; // Move white pointer as well
+
+        } else if (colors[whitePointer] === 2) {
+            swapColors(colors, whitePointer, bluePointer);
+            bluePointer--;
+
+        } else {
+            whitePointer++;
+        }
+    }
+
+    return colors;
+}
+
+console.log(sortColors([0,1,0]))
