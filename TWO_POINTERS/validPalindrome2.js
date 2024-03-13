b3748f7c82fd80d5df7fadb4b72dd45957a2bb01
Manzar-Hassan <manzarhassan05@gmail.com>
2024-03-13 14:50:27 +0530

    adds valid palindrome 2 code

---
+++ TWO_POINTERS/validPalindrome2.js
@@ -0,0 +1,43 @@
+function checkPalindrome(str) {
+    let startPointer = 0;
+    let endPointer = str.length - 1;
+
+    while(startPointer < endPointer) {
+        if(str[startPointer] === str[endPointer]) {
+            startPointer++;
+            endPointer--;
+
+        } else {
+            return false
+        }
+    }
+
+    return true
+}
+
+function isPalindrome(s) {
+    let startPointer = 0;
+    let endPointer = s.length - 1;
+
+    while(startPointer < endPointer) {
+        if(s[startPointer] === s[endPointer]) {
+            startPointer++;
+            endPointer--;
+        
+        } else {
+            if(checkPalindrome(s.substr(startPointer, endPointer - startPointer))) {
+                return true
+
+            } else if(checkPalindrome(s.substr(startPointer + 1, endPointer - startPointer))) {
+                return true
+
+            } else {
+                return false
+            } 
+        }
+    }
+
+    return true;
+}
+
+console.log(isPalindrome('dead'))
