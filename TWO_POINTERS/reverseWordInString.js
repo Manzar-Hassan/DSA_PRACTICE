663292cd075bf64f38ceeeb7acaa09f734d8310b
Manzar-Hassan <manzarhassan05@gmail.com>
2024-03-13 11:34:09 +0530

    adds reverse word in string code

---
+++ TWO_POINTERS/reverseWordInString.js
@@ -0,0 +1,26 @@
+function reverseWords(str) {
+    str = str.split('').reverse().join('');
+    
+    let start = 0;
+    let end = 0;
+    let result = '';
+
+    while (end < str.length) {
+        while (end < str.length && str[end] !== ' ') {
+            end++;
+        }
+
+        let word = str.substring(start, end);
+        result += word.split('').reverse().join('') + ' ';
+
+        while (end < str.length && str[end] === ' ') {
+            end++;
+        }
+        start = end;
+    }
+
+    return result.trim();
+}
+
+console.log(reverseWords("Hello  world"));
+console.log(reverseWords("cat fish"));
