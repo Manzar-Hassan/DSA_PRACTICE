455fd9e4174c5b0506f4f1e83b922a6f19b902a4
Manzar-Hassan <manzarhassan05@gmail.com>
2024-03-14 09:39:28 +0530

    changes

--- TWO_POINTERS/removeNthNodeFromEnd.js
+++ TWO_POINTERS/removeNthNodeFromEnd.js
@@ -48,27 +48,22 @@ function removeNthLastNode(head, n) {
     let fast = head;
     let slow = head;
 
-    // Move the fast pointer forward by n steps
     for (let i = 0; i < n; i++) {
         if (fast === null) {
-            // If n is greater than the length of the list, return head
             return head;
         }
         fast = fast.next;
     }
 
-    // If fast becomes null, it means the node to be removed is the head
     if (fast === null) {
         return head.next;
     }
 
-    // Move fast to the end, maintaining the gap of n between fast and slow
     while (fast.next !== null) {
         fast = fast.next;
         slow = slow.next;
     }
 
-    // Remove the nth last node by updating the next pointer of the node before it
     slow.next = slow.next.next;
 
     return head;
@@ -76,8 +71,8 @@ function removeNthLastNode(head, n) {
 
 let list = new LinkedList();
 list.createLinkedList([23,28,10,5,67,39,70,28]);
-console.log(list.display()); // Display the initial list
+console.log(list.display());
 
 removeNthLastNode(list.head, 2);
 
-console.log(list.display()); // Display the list after removing the 2nd last node
+console.log(list.display());
